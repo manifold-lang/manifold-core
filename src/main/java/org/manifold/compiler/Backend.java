1d92b316afc001cb527bd598c3774cc439438632
Murphy Berzish <murphy.berzish@gmail.com>
2014-09-10 13:09:51 -0400

    checkstyle tho

--- src/main/java/org/manifold/compiler/Backend.java
+++ src/main/java/org/manifold/compiler/Backend.java
@@ -5,5 +5,5 @@ import org.manifold.compiler.middle.Schematic;
 public interface Backend {
   public String getBackendName();
   public void invokeBackend(Schematic schematic, String args[]) 
-    throws Exception;
+      throws Exception;
 }
