7c230ffea0b4be8f7679c61d98d20d674be4219d
Murphy Berzish <murphy.berzish@gmail.com>
2014-09-10 09:25:29 -0400

    add invocation function to interface

--- src/main/java/org/manifold/compiler/Frontend.java
+++ src/main/java/org/manifold/compiler/Frontend.java
@@ -1,5 +1,8 @@
 package org.manifold.compiler;
 
+import org.manifold.compiler.middle.Schematic;
+
 public interface Frontend {
-  public String getName();
+  public String getFrontendName();
+  public Schematic invokeFrontend(String args[]) throws Exception;
 }
