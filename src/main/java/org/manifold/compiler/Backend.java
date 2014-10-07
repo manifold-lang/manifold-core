4165a17386f01fe8ba854eb163723e3bcb5ff54d
Murphy Berzish <murphy.berzish@gmail.com>
2014-10-07 15:34:21 -0400

    integrate service interfaces with Commons-CLI argument handling

--- src/main/java/org/manifold/compiler/Backend.java
+++ src/main/java/org/manifold/compiler/Backend.java
@@ -1,9 +1,12 @@
 package org.manifold.compiler;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
 import org.manifold.compiler.middle.Schematic;
 
 public interface Backend {
   public String getBackendName();
-  public void invokeBackend(Schematic schematic, String args[]) 
+  public void registerArguments(Options options);
+  public void invokeBackend(Schematic schematic, CommandLine cmdline) 
       throws Exception;
 }
--- src/main/java/org/manifold/compiler/Frontend.java
+++ src/main/java/org/manifold/compiler/Frontend.java
@@ -1,8 +1,11 @@
 package org.manifold.compiler;
 
 import org.manifold.compiler.middle.Schematic;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
 
 public interface Frontend {
   public String getFrontendName();
-  public Schematic invokeFrontend(String args[]) throws Exception;
+  public void registerArguments(Options options);
+  public Schematic invokeFrontend(CommandLine cmdline) throws Exception;
 }
