9f0503181edbf597df455556dc49a8f62d5cdf51
Murphy Berzish <murphy.berzish@gmail.com>
2015-05-12 09:31:39 -0400

    backannotation of node attributes

--- src/main/java/org/manifold/compiler/middle/BackAnnotationBuilder.java
+++ src/main/java/org/manifold/compiler/middle/BackAnnotationBuilder.java
@@ -3,14 +3,17 @@ package org.manifold.compiler.middle;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.manifold.compiler.ConnectionValue;
 import org.manifold.compiler.ConstraintType;
 import org.manifold.compiler.NodeTypeValue;
 import org.manifold.compiler.NodeValue;
 import org.manifold.compiler.PortTypeValue;
+import org.manifold.compiler.PortValue;
 import org.manifold.compiler.TypeMismatchException;
 import org.manifold.compiler.TypeValue;
 import org.manifold.compiler.UndeclaredAttributeException;
 import org.manifold.compiler.UndeclaredIdentifierException;
+import org.manifold.compiler.UndefinedBehaviourError;
 import org.manifold.compiler.UserDefinedTypeValue;
 import org.manifold.compiler.Value;
 
@@ -63,6 +66,21 @@ public class BackAnnotationBuilder {
     nodeAttributeAnnotations.get(nodeName).put(attrName, attrValue);
   }
 
+  private String getPortName(PortValue port) {
+    String portName = null;
+    for (Map.Entry<String, PortValue> entry
+        : port.getParent().getPorts().entrySet()) {
+      if (entry.getValue().equals(port)) {
+        portName = entry.getKey();
+        break;
+      }
+    }
+    if (portName == null) {
+      throw new UndefinedBehaviourError("cannot find name of port");
+    }
+    return portName;
+  }
+
   public Schematic build() throws SchematicException {
     Schematic newSchematic = new Schematic(originalSchematic.getName());
     // start by copying all the type declarations from the old schematic
@@ -86,6 +104,73 @@ public class BackAnnotationBuilder {
       newSchematic.addConstraintType(constraintType.getKey(),
           constraintType.getValue());
     }
+    Map<NodeValue, NodeValue> nodeIsomorphisms = new HashMap<>();
+    for (Map.Entry<String, NodeValue> originalEntry : originalSchematic
+        .getNodes().entrySet()) {
+      String nodeName = originalEntry.getKey();
+      NodeValue originalNode = originalEntry.getValue();
+
+      boolean modifiedNode;
+      boolean modifiedPorts;
+      modifiedNode = (nodeAttributeAnnotations.containsKey(nodeName));
+      modifiedPorts = false; // TODO port attribute edits
+      if (modifiedNode || modifiedPorts) {
+        NodeTypeValue nodeType = (NodeTypeValue) originalNode.getType();
+        // apply all node attribute edits
+        Map<String, Value> originalAttributes = originalNode.getAttributes()
+            .getAll();
+        Map<String, Value> nodeAttributes = new HashMap<>();
+        for (String attrName : originalAttributes.keySet()) {
+          if (nodeAttributeAnnotations.containsKey(nodeName)
+              && nodeAttributeAnnotations.get(nodeName).containsKey(attrName)) {
+            // use new value
+            nodeAttributes.put(attrName, nodeAttributeAnnotations.get(nodeName)
+                .get(attrName));
+          } else {
+            // use original value
+            nodeAttributes.put(attrName, originalAttributes.get(attrName));
+          }
+        }
+        // TODO port attribute edits
+        Map<String, Map<String, Value>> portAttributes = new HashMap<>();
+        for (String portName : originalNode.getPorts().keySet()) {
+          PortValue originalPort = originalNode.getPort(portName);
+          portAttributes.put(portName, originalPort.getAttributes().getAll());
+        }
+
+        // create a new node with the new attributes
+        NodeValue newNode = new NodeValue(
+            nodeType, nodeAttributes, portAttributes);
+        nodeIsomorphisms.put(originalNode, newNode);
+        newSchematic.addNode(nodeName, newNode);
+      } else {
+        // no edits; pass it through
+        nodeIsomorphisms.put(originalNode, originalNode);
+        newSchematic.addNode(nodeName, originalNode);
+      }
+    }
+    Map<ConnectionValue, ConnectionValue> connectionIsomorphisms =
+        new HashMap<>();
+    for (Map.Entry<String, ConnectionValue> originalEntry : originalSchematic
+        .getConnections().entrySet()) {
+      String connectionName = originalEntry.getKey();
+      ConnectionValue originalConnection = originalEntry.getValue();
+      // the easiest thing to do is recreate every connection
+      // with respect to the node isomorphisms
+      PortValue originalPortFrom = originalConnection.getFrom();
+      PortValue newPortFrom = nodeIsomorphisms.get(originalPortFrom.getParent())
+          .getPort(getPortName(originalPortFrom));
+      PortValue originalPortTo = originalConnection.getTo();
+      PortValue newPortTo = nodeIsomorphisms.get(originalPortTo.getParent())
+          .getPort(getPortName(originalPortTo));
+      Map<String, Value> newAttributes = originalConnection.getAttributes()
+          .getAll(); // TODO connection attribute edits
+      ConnectionValue newConnection = new ConnectionValue(
+          newPortFrom, newPortTo, newAttributes);
+      newSchematic.addConnection(connectionName, newConnection);
+      connectionIsomorphisms.put(originalConnection, newConnection);
+    }
+    // TODO constraints
     return newSchematic;
   }
 
